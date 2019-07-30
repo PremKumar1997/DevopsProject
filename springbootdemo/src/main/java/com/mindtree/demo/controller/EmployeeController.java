ab1742de087b498720ceee012682c7f0f9bd50fd
PremKumar <Prem.Kumar4@mindtree.com>
2019-07-30 10:52:53 +0530

    Sample Spring boot Project

---
+++ springbootdemo/src/main/java/com/mindtree/demo/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.mindtree.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindtree.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"Prem","Kumar","abc@gmail.com"));
+		return employeesList;
+    }
+
+}
